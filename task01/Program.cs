bf0f8f1f557f0a4e4d0879adae56c8dacffb64b4
Timur <temuchin@ya.ru>
2022-09-22 19:12:25 +0400

    all changes

---
+++ task01/Program.cs
@@ -0,0 +1,30 @@
+﻿/*  Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+int a, b;
+ 
+        Console.WriteLine("Введите два числа: ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
---
+++ task02/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+int a, b, c;
+
+ 
+        Console.WriteLine("Введите три числа: ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+
+        c = Convert.ToInt32(Console.ReadLine());
+ 
+       if (a > b && a > c)
+       {
+
+                    Console.WriteLine("{0} наибольшее число ", a);
+       }
+
+                else if (b > c)
+                {
+
+                    Console.WriteLine("{0} наибольшее число ", b);
+                }
+
+                else
+                {
+
+                    Console.WriteLine("{0} наибольшее число ", c);
+                }          
+           
